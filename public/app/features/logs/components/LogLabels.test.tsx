f3abd3b0f072dc09fced71d2d7b7a187d743ea2c
Matias Chomicki <matyax@gmail.com>
2024-12-01 11:30:40 +0100

    Address lint issues

--- public/app/features/logs/components/LogLabels.test.tsx
+++ public/app/features/logs/components/LogLabels.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
+import { LOG_LINE_BODY_FIELD_NAME } from './LogDetailsBody';
 import { LogLabels, LogLabelsList } from './LogLabels';
 
 describe('<LogLabels />', () => {
@@ -27,8 +28,9 @@ describe('<LogLabels />', () => {
 
 describe('<LogLabelsList />', () => {
   it('renders labels', () => {
-    render(<LogLabelsList labels={['bar', '42']} />);
+    render(<LogLabelsList labels={['bar', '42', LOG_LINE_BODY_FIELD_NAME]} />);
     expect(screen.queryByText('bar')).toBeInTheDocument();
     expect(screen.queryByText('42')).toBeInTheDocument();
+    expect(screen.queryByText('log line')).toBeInTheDocument();
   });
 });
